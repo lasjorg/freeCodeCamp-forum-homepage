4cce2b1c86643c19b3dfb79a4306c848312057e5
lasjorg <28780271+lasjorg@users.noreply.github.com>
2022-07-07 20:31:32 +0200

    Initial commit

---
+++ src/routes/ForumInterface.ts
@@ -0,0 +1,103 @@
+export interface Forum {
+	users: User[];
+	primary_groups: PrimaryGroup[];
+	flair_groups: FlairGroup[];
+	topic_list: TopicList;
+}
+
+export interface FlairGroup {
+	id: number;
+	name: string;
+	flair_url: string;
+	flair_bg_color: string;
+	flair_color: string;
+}
+
+export interface PrimaryGroup {
+	id: number;
+	name: string;
+}
+
+export interface TopicList {
+	can_create_topic: boolean;
+	more_topics_url: string;
+	per_page: number;
+	topics: Topic[];
+}
+
+export interface Topic {
+	id: number;
+	title: string;
+	fancy_title: string;
+	slug: string;
+	posts_count: number;
+	reply_count: number;
+	highest_post_number: number;
+	image_url: null | string;
+	created_at: Date;
+	last_posted_at: Date;
+	bumped: boolean;
+	bumped_at: Date;
+	archetype: Archetype;
+	unseen: boolean;
+	pinned: boolean;
+	unpinned: null;
+	excerpt?: string;
+	visible: boolean;
+	closed: boolean;
+	archived: boolean;
+	bookmarked: null;
+	liked: null;
+	tags_descriptions: Record<string, never>;
+	views: number;
+	like_count: number;
+	has_summary: boolean;
+	last_poster_username: string;
+	category_id: number;
+	pinned_globally: boolean;
+	featured_link: null;
+	has_accepted_answer: boolean;
+	can_vote: boolean;
+	posters: Poster[];
+}
+
+export enum Archetype {
+	Regular = 'regular'
+}
+
+export interface Poster {
+	extras: Extras | null;
+	description: Description;
+	user_id: number;
+	primary_group_id: number | null;
+	flair_group_id: number | null;
+}
+
+export enum Description {
+	FrequentPoster = 'Frequent Poster',
+	FrequentPosterAcceptedAnswer = 'Frequent Poster, Accepted Answer',
+	MostRecentPoster = 'Most Recent Poster',
+	MostRecentPosterAcceptedAnswer = 'Most Recent Poster, Accepted Answer',
+	OriginalPoster = 'Original Poster',
+	OriginalPosterMostRecentPoster = 'Original Poster, Most Recent Poster'
+}
+
+export enum Extras {
+	Latest = 'latest',
+	LatestSingle = 'latest single'
+}
+
+export interface User {
+	id: number;
+	username: string;
+	name: string;
+	avatar_template: string;
+	primary_group_name?: string;
+	flair_name: null | string;
+	flair_url?: string;
+	flair_bg_color?: string;
+	flair_color?: string;
+	admin?: boolean;
+	moderator?: boolean;
+	trust_level: number;
+}
---
+++ src/routes/index.svelte
@@ -0,0 +1,134 @@
+<script context="module" lang="ts">
+	import type { User, Topic } from './ForumInterface';
+
+	export async function load() {
+		const response = await fetch(`https://forum-proxy.freecodecamp.rocks/latest`);
+		const JSON = await response.json();
+		console.log('JSON', JSON);
+		if (response.ok) {
+			return {
+				status: response.status,
+				props: {
+					users: JSON.users,
+					topics: JSON.topic_list.topics
+				}
+			};
+		}
+	}
+</script>
+
+<script lang="ts">
+	import { formatDistanceStrict } from 'date-fns';
+	export let users: User[];
+	export let topics: Topic[];
+
+	const getAvatar = (user: User | undefined) => {
+		if (user?.avatar_template) {
+			if (user?.avatar_template.includes('discourse')) {
+				// "https://avatars.discourse-cdn.com/v4/letter/e/d07c76/{size}.png"
+				return user?.avatar_template.replace('{size}', '25');
+			}
+			return `https://forum.freecodecamp.org/${user?.avatar_template.replace('{size}', '25')}`;
+		}
+	};
+
+	const getUser = (userId: number): User | undefined => {
+		return users?.find((user) => user?.id === userId);
+	};
+
+	const formatTime = (word: string) => {
+		const [time, words] = word.split(' ');
+		return `${time}${words[0]}`;
+	};
+</script>
+
+<div class="container">
+	{#if topics}
+		<div class="details">
+			<h4>Topic</h4>
+			<h4>Replies</h4>
+			<h4>Views</h4>
+			<h4>Activity</h4>
+		</div>
+		{#each topics as topic}
+			<div class="topic">
+				<div class="topic-title">
+					<a href={`https://forum.freecodecamp.org/t/${topic.slug}`}>{topic.title}</a>
+				</div>
+				<div class="topic-posters">
+					{#each topic.posters as poster}
+						<div class="poster">
+							<a href={`https://forum.freecodecamp.org/u/${getUser(poster.user_id)?.username}`}>
+								<img src={getAvatar(getUser(poster.user_id))} alt="avatar" />
+							</a>
+						</div>
+					{/each}
+				</div>
+				<div class="replies">
+					{topic.reply_count}
+				</div>
+				<div class="views">
+					{topic.views}
+				</div>
+				<div class="topic-date">
+					{formatTime(formatDistanceStrict(new Date(topic.bumped_at), new Date()))}
+				</div>
+			</div>
+		{/each}
+	{/if}
+</div>
+
+<style>
+	.container {
+		width: min(1110px, 90%);
+		margin-inline: auto;
+		min-width: 520px;
+		color: rgb(135, 136, 147);
+	}
+
+	.details {
+		display: grid;
+		grid-template-columns: 1fr auto auto auto 1rem;
+		gap: 1.3rem;
+		border-bottom: 3px solid #424255;
+	}
+
+	.topic {
+		display: grid;
+		grid-template-columns: 1fr auto 2.5rem 2.5rem 6.25rem;
+		gap: 2rem;
+		padding-block: 1.2rem;
+		border-bottom: 1px solid #424255;
+		align-items: center;
+	}
+
+	.topic-posters {
+		display: flex;
+		gap: 0.3rem;
+		margin-left: auto;
+	}
+
+	.replies,
+	.views,
+	.topic-date {
+		flex: 0 1 80px;
+		color: rgb(135, 136, 147);
+		text-align: center;
+	}
+
+	.topic-title {
+		flex: 1;
+	}
+
+	a {
+		text-decoration: none;
+		color: #dddee0;
+		font-family: inherit;
+		font-size: 1.4rem;
+	}
+	@media (max-width: 768px) {
+		a {
+			font-size: 1rem;
+		}
+	}
+</style>
